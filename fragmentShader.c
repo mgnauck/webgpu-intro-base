37a14fae97070b39dcfc8e3c3ae28ca2f1357604
Markus Gnauck <m.gnauck@xceptance.net>
2023-02-20 13:44:06 +0100

    Tab stops

--- fragmentShader.c
+++ fragmentShader.c
@@ -1,12 +1,12 @@
 struct Uniforms
 {
-	resolution : vec2<f32>,
-	time : f32,
+  resolution : vec2<f32>,
+  time : f32,
 }
 @binding(0) @group(0) var<uniform> uniforms : Uniforms;
 
 @fragment
 fn main( @builtin(position) position : vec4<f32>)  -> @location(0) vec4<f32>
 {
-	return vec4<f32>( vec3<f32>( clamp( sin( uniforms.time * 0.001 ), 0.0, 1.0 ) ), 1.0 );
+  return vec4<f32>( vec3<f32>( clamp( sin( uniforms.time * 0.001 ), 0.0, 1.0 ) ), 1.0 );
 }
