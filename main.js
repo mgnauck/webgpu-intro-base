8e365b8c374a567456cbb3663b4f3e7170b6b42b
Markus Gnauck <m.gnauck@xceptance.net>
2023-03-21 12:23:02 +0100

    Clean up

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ const BPM: f32 = 160.0;
 const PI: f32 = 3.141592654;
 const TAU: f32 = 6.283185307;
 
-fn time_to_beat(t: f32) -> f32 {
+fn timeToBeat(t: f32) -> f32 {
   return t / 60.0 * BPM;
 }
 
-fn beat_to_time(b: f32) -> f32 {
+fn beatToTime(b: f32) -> f32 {
   return b / BPM * 60.0;
 }
 
@@ -40,7 +40,7 @@ fn kick(time: f32) -> f32 {
   return amp * sine(phase);
 }
 
-fn hi_hat(time: f32) -> vec2<f32> {
+fn hiHat(time: f32) -> vec2<f32> {
   let amp: f32 = exp(-40.0 * time);
   return amp * noise(time * 110.0).xy;
 }
@@ -48,24 +48,24 @@ fn hi_hat(time: f32) -> vec2<f32> {
 @group(0) @binding(0) var outputTexture: texture_storage_2d<rg32float, write>;
 
 @compute @workgroup_size(8, 8)
-fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+fn main(@builtin(global_invocation_id) globalId: vec3<u32>) {
   
-  if (global_id.x >= ${AUDIO_WIDTH} || global_id.y >= ${AUDIO_HEIGHT}) {
+  if (globalId.x >= ${AUDIO_WIDTH} || globalId.y >= ${AUDIO_HEIGHT}) {
     return;
   }
 
-  let time: f32 = f32(${AUDIO_WIDTH} * global_id.y + global_id.x) / 44100.0;
-  let beat: f32 = time_to_beat(time);
+  let time: f32 = f32(${AUDIO_WIDTH} * globalId.y + globalId.x) / 44100.0;
+  let beat: f32 = timeToBeat(time);
 
   // Kick
-  var res = vec2<f32>(0.6 * kick(beat_to_time(beat % 1.0)));
+  var res = vec2<f32>(0.6 * kick(beatToTime(beat % 1.0)));
 
   // Hihat
-  res += 0.3 * hi_hat(beat_to_time((beat + 0.5) % 1.0));
+  res += 0.3 * hiHat(beatToTime((beat + 0.5) % 1.0));
 
   textureStore(
     outputTexture,
-    vec2<u32>(global_id.x, global_id.y),
+    vec2<u32>(globalId.x, globalId.y),
     vec4<f32>(clamp(res, vec2<f32>(-1.0), vec2<f32>(1.0)), 0.0, 1.0));
 }
 `;
@@ -80,18 +80,19 @@ struct Uniforms {
 @group(0) @binding(1) var<uniform> uniforms: Uniforms;
 
 @compute @workgroup_size(8, 8)
-fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+fn main(@builtin(global_invocation_id) globalId: vec3<u32>) {
 
-  if (global_id.x >= ${CANVAS_WIDTH} || global_id.y >= ${CANVAS_HEIGHT}) {
+  if (globalId.x >= ${CANVAS_WIDTH} || globalId.y >= ${CANVAS_HEIGHT}) {
     return;
   }
 
-  var uv = vec2<f32>(f32(global_id.x) / ${CANVAS_WIDTH}.0, f32(global_id.y) / ${CANVAS_HEIGHT}.0);
-  var col = vec3<f32>(0.5 + 0.5 * cos(uniforms.time + uv.xyx + vec3<f32>(0.0, 2.0, 4.0)));
+  let uv = vec2<f32>(f32(globalId.x) / ${CANVAS_WIDTH}.0, f32(globalId.y) / ${
+    CANVAS_HEIGHT}.0);
+  let col = vec3<f32>(0.5 + 0.5 * cos(uniforms.time + uv.xyx + vec3<f32>(0.0, 2.0, 4.0)));
 
   textureStore(
     outputTexture,
-    vec2<u32>(global_id.x, global_id.y),
+    vec2<u32>(globalId.x, globalId.y),
     vec4<f32>(col, 1.0));
 }
 `;
@@ -103,15 +104,15 @@ struct Output {
 }
 
 @vertex
-fn main(@builtin(vertex_index) vertex_index: u32) -> Output {
+fn main(@builtin(vertex_index) vertexIndex: u32) -> Output {
 
-  var pos = array<vec2<f32>, 4>(
+  let pos = array<vec2<f32>, 4>(
     vec2(-1.0, 1.0), vec2(-1.0, -1.0), vec2(1.0, 1.0), vec2(1.0, -1.0));
   
   var output: Output;
 
-  output.position = vec4<f32>(pos[vertex_index], 0.0, 1.0);
-  output.texCoord = pos[vertex_index] * vec2<f32>(0.5, -0.5) + vec2<f32>(0.5, 0.5);
+  output.position = vec4<f32>(pos[vertexIndex], 0.0, 1.0);
+  output.texCoord = pos[vertexIndex] * vec2<f32>(0.5, -0.5) + vec2<f32>(0.5, 0.5);
 
   return output;
 }
@@ -143,20 +144,18 @@ let blitPipeline;
 
 let canvas;
 let context;
-let presentationFormat;
 
 let start;
 
 function setupCanvasAndContext() {
-  
   document.body.innerHTML = "<button>CLICK<canvas style='width:0;cursor:none'>";
   canvas = document.querySelector("canvas");
   canvas.width = CANVAS_WIDTH;
   canvas.height = CANVAS_HEIGHT;
 
-  presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  let presentationFormat = navigator.gpu.getPreferredCanvasFormat();
   if (presentationFormat !== "bgra8unorm") {
-    throw new Error(`Expected canvas pixel format of 'bgra8unorm' but
+    throw new Error(`Expected canvas pixel format of bgra8unorm but
        was '${presentationFormat}'.`);
   }
 
@@ -166,16 +165,14 @@ function setupCanvasAndContext() {
 }
 
 function writeBufferData(buffer, data) {
- 
   const bufferData = new Float32Array(data);
-  
+
   device.queue.writeBuffer(
       buffer, 0, bufferData.buffer, bufferData.byteOffset,
       bufferData.byteLength);
 }
 
 async function createComputePipeline(shaderCode, bindGroupLayout) {
-
   return device.createComputePipelineAsync({
     layout: (bindGroupLayout === undefined) ?
         "auto" :
@@ -188,8 +185,7 @@ async function createComputePipeline(shaderCode, bindGroupLayout) {
 }
 
 function encodeComputePassAndSubmit(
-  pipeline, bindGroup, workgroupCountX, workgroupCountY, preSubmitOperation) {
-  
+    pipeline, bindGroup, workgroupCountX, workgroupCountY, preSubmitOperation) {
   const commandEncoder = device.createCommandEncoder();
 
   const passEncoder = commandEncoder.beginComputePass();
@@ -198,7 +194,7 @@ function encodeComputePassAndSubmit(
   passEncoder.dispatchWorkgroups(workgroupCountX, workgroupCountY);
   passEncoder.end();
 
-  if(preSubmitOperation != undefined) {
+  if (preSubmitOperation != undefined) {
     preSubmitOperation(commandEncoder);
   }
 
@@ -217,8 +213,7 @@ function createRenderPassDescriptor(view) {
 }
 
 async function createRenderPipeline(
-    vertexShaderCode, fragmentShaderCode, presentationFormat, bindGroupLayout) {
-
+    vertexShaderCode, fragmentShaderCode, bindGroupLayout) {
   return device.createRenderPipelineAsync({
     layout: (bindGroupLayout === undefined) ?
         "auto" :
@@ -230,14 +225,13 @@ async function createRenderPipeline(
     fragment: {
       module: device.createShaderModule({code: fragmentShaderCode}),
       entryPoint: "main",
-      targets: [{format: presentationFormat}]
+      targets: [{format: "bgra8unorm"}]
     },
     primitive: {topology: "triangle-strip"}
   });
 }
 
 function encodeBlitPassAndSubmit(passDescriptor, pipeline, bindGroup) {
-  
   const commandEncoder = device.createCommandEncoder();
 
   const passEncoder = commandEncoder.beginRenderPass(passDescriptor);
@@ -249,44 +243,7 @@ function encodeBlitPassAndSubmit(passDescriptor, pipeline, bindGroup) {
   device.queue.submit([commandEncoder.finish()]);
 }
 
-function render(time) {
-
-  if (audioContext === undefined && start === undefined) {
-    start = time;
-  }
-
-  writeBufferData(contentUniformBuffer, [
-    CANVAS_WIDTH, CANVAS_HEIGHT,
-    AUDIO ? audioContext.currentTime : ((time - start) / 1000.0), 0.0
-  ]);
-
-  encodeComputePassAndSubmit(
-    contentPipeline, contentBindGroup, 
-    Math.ceil(CANVAS_WIDTH / 8), Math.ceil(CANVAS_HEIGHT / 8));
-
-  blitPassDescriptor.colorAttachments[0].view =
-      context.getCurrentTexture().createView();
-  encodeBlitPassAndSubmit(blitPassDescriptor, blitPipeline, blitBindGroup);
-
-  requestAnimationFrame(render);
-}
-
-function setupPerformanceTimer(timerName) {
-  
-  let begin = performance.now();
-  
-  device.queue.onSubmittedWorkDone()
-      .then(function() {
-        let end = performance.now();
-        console.log(`${timerName} (ms): ${(end - begin).toFixed(2)}`);
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-}
-
 async function renderAudio() {
- 
   audioContext = new AudioContext();
 
   let audioBuffer = audioContext.createBuffer(
@@ -295,7 +252,7 @@ async function renderAudio() {
       "Max audio length: " +
       (audioBuffer.length / audioContext.sampleRate / 60).toFixed(2) + " min");
 
-   let audioTexture = device.createTexture({
+  let audioTexture = device.createTexture({
     format: "rg32float",
     usage: GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.COPY_SRC,
     size: [AUDIO_WIDTH, AUDIO_HEIGHT]
@@ -322,15 +279,14 @@ async function renderAudio() {
   setupPerformanceTimer("Render audio");
 
   encodeComputePassAndSubmit(
-    await createComputePipeline(audioShader, audioBindGroupLayout),
-    audioBindGroup,
-    Math.ceil(AUDIO_WIDTH / 8), Math.ceil(AUDIO_HEIGHT / 8),
-    function(commandEncoder) {
-      commandEncoder.copyTextureToBuffer(
-        {texture: audioTexture},
-        {buffer: readBuffer, bytesPerRow: AUDIO_WIDTH * 2 * 4},
-        [AUDIO_WIDTH, AUDIO_HEIGHT]);  
-    });
+      await createComputePipeline(audioShader, audioBindGroupLayout),
+      audioBindGroup, Math.ceil(AUDIO_WIDTH / 8), Math.ceil(AUDIO_HEIGHT / 8),
+      function(commandEncoder) {
+        commandEncoder.copyTextureToBuffer(
+            {texture: audioTexture},
+            {buffer: readBuffer, bytesPerRow: AUDIO_WIDTH * 2 * 4},
+            [AUDIO_WIDTH, AUDIO_HEIGHT]);
+      });
 
   await readBuffer.mapAsync(GPUMapMode.READ);
   const audioData = new Float32Array(readBuffer.getMappedRange());
@@ -338,8 +294,7 @@ async function renderAudio() {
   const channel0 = audioBuffer.getChannelData(0);
   const channel1 = audioBuffer.getChannelData(1);
 
-  let i;
-  for (i = 0; i < AUDIO_WIDTH * AUDIO_HEIGHT; i += 1) {
+  for (let i = 0; i < AUDIO_WIDTH * AUDIO_HEIGHT; i += 1) {
     channel0[i] = audioData[(i << 1) + 0];
     channel1[i] = audioData[(i << 1) + 1];
   }
@@ -352,7 +307,6 @@ async function renderAudio() {
 }
 
 async function prepareContentResources() {
-
   let contentTexture = device.createTexture({
     format: "rgba8unorm",
     usage: GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING,
@@ -382,63 +336,73 @@ async function prepareContentResources() {
   contentBindGroup = device.createBindGroup({
     layout: contentBindGroupLayout,
     entries: [
-      {
-        binding: 0,
-        resource: contentTextureView
-      },
-      {
-        binding: 1,
-        resource: {buffer: contentUniformBuffer}
-      }
+      {binding: 0, resource: contentTextureView},
+      {binding: 1, resource: {buffer: contentUniformBuffer}}
     ]
   });
-  
-  contentPipeline = await createComputePipeline(
-    contentShader, contentBindGroupLayout);
+
+  contentPipeline =
+      await createComputePipeline(contentShader, contentBindGroupLayout);
 }
 
 async function prepareBlitResources() {
-
-  let blitSampler = device.createSampler({
-    magFilter: "linear"
-  });
+  let blitSampler = device.createSampler({magFilter: "linear"});
 
   let blitBindGroupLayout = device.createBindGroupLayout({
     entries: [
-      {
-        binding: 0,
-        visibility: GPUShaderStage.FRAGMENT,
-        texture: {}
-      },
-      {
-        binding: 1,
-        visibility: GPUShaderStage.FRAGMENT,
-        sampler: {}
-      }
+      {binding: 0, visibility: GPUShaderStage.FRAGMENT, texture: {}},
+      {binding: 1, visibility: GPUShaderStage.FRAGMENT, sampler: {}}
     ]
   });
 
   blitBindGroup = device.createBindGroup({
     layout: blitBindGroupLayout,
     entries: [
-      {
-        binding: 0,
-        resource: contentTextureView
-      },
-      {
-        binding: 1,
-        resource: blitSampler
-      }
+      {binding: 0, resource: contentTextureView},
+      {binding: 1, resource: blitSampler}
     ]
   });
 
   blitPassDescriptor = createRenderPassDescriptor(undefined);
-  blitPipeline = await createRenderPipeline(
-      vertexShader, blitShader, presentationFormat, blitBindGroupLayout);
+  blitPipeline =
+      await createRenderPipeline(vertexShader, blitShader, blitBindGroupLayout);
+}
+
+function render(time) {
+  if (audioContext === undefined && start === undefined) {
+    start = time;
+  }
+
+  writeBufferData(contentUniformBuffer, [
+    CANVAS_WIDTH, CANVAS_HEIGHT,
+    AUDIO ? audioContext.currentTime : ((time - start) / 1000.0), 0.0
+  ]);
+
+  encodeComputePassAndSubmit(
+      contentPipeline, contentBindGroup, Math.ceil(CANVAS_WIDTH / 8),
+      Math.ceil(CANVAS_HEIGHT / 8));
+
+  blitPassDescriptor.colorAttachments[0].view =
+      context.getCurrentTexture().createView();
+  encodeBlitPassAndSubmit(blitPassDescriptor, blitPipeline, blitBindGroup);
+
+  requestAnimationFrame(render);
+}
+
+function setupPerformanceTimer(timerName) {
+  let begin = performance.now();
+
+  device.queue.onSubmittedWorkDone()
+      .then(function() {
+        let end = performance.now();
+        console.log(`${timerName} (ms): ${(end - begin).toFixed(2)}`);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
 }
 
 function startRender() {
-
   if (FULLSCREEN) {
     canvas.requestFullscreen();
   } else {
@@ -457,7 +421,6 @@ function startRender() {
 }
 
 async function main() {
-
   if (!navigator.gpu) {
     throw new Error("WebGPU is not supported on this browser.");
   }
@@ -475,13 +438,13 @@ async function main() {
   if (AUDIO) {
     await renderAudio();
   }
-  
-  setupCanvasAndContext(); 
- 
+
   await prepareContentResources();
   await prepareBlitResources();
-  
-  if(AUDIO) {
+
+  setupCanvasAndContext();
+
+  if (AUDIO) {
     document.querySelector("button").addEventListener("click", startRender);
   } else {
     startRender();
