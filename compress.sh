e79a62d1c26b82e6b6ba35d6b4bd473fce5ef00d
Markus Gnauck <17846097+mgnauck@users.noreply.github.com>
2023-09-27 21:41:32 +0200

    Not copying player to output

--- compress.sh
+++ compress.sh
@@ -13,14 +13,13 @@ shader_excludes=$2
 
 rm -rf $output_dir
 mkdir $output_dir
-cp $infile $output_dir
 
 ./wgslminify -e $shader_excludes audio.wgsl > $output_dir/audio_minified.wgsl
 ./wgslminify -e $shader_excludes visual.wgsl > $output_dir/visual_minified.wgsl
 
 cd $output_dir
 
-sed -f ../compress.sed $infile > ${infile_name}_with_shaders.${infile_ext}
+sed -f ../compress.sed ../$infile > ${infile_name}_with_shaders.${infile_ext}
 
 terser ${infile_name}_with_shaders.${infile_ext} --mangle --rename --toplevel --c toplevel,passes=5,unsafe=true,pure_getters=true > ${infile_name}_minified.${infile_ext}
 
