7565c1c89e0598d5c414f948e80c25ad2751ad2b
Markus Gnauck <17846097+mgnauck@users.noreply.github.com>
2023-10-01 21:07:31 +0200

    Zopfli rounds upped

--- compress.sh
+++ compress.sh
@@ -30,4 +30,4 @@ sed -f ../compress.sed ../$infile > ${infile_name}_with_shaders.${infile_ext}
 
 terser ${infile_name}_with_shaders.${infile_ext} -m -c toplevel,passes=5,drop_console=true,unsafe=true,pure_getters=true,keep_fargs=false,booleans_as_integers=true --toplevel > ${infile_name}_minified.${infile_ext}
 
-../js-payload-compress --zopfli-iterations=50 ${infile_name}_minified.${infile_ext} ${infile_name}_compressed.html
+../js-payload-compress --zopfli-iterations=100 ${infile_name}_minified.${infile_ext} ${infile_name}_compressed.html
